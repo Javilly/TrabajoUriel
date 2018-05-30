7e4f078336a8b90150f650f43f2dc5a10b085aac
JAVI\Javier <javi.valda@gmail.com>
2018-05-30 00:16:02 -0300

    Empezando script del spawn de plataforma

---
+++ FinalUriel/Assets/Scripts/SpawnPlataformas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlataformas : MonoBehaviour
+{
+
+    public Vector3 posInicial;
+    public Vector3 posArriba;
+    public Vector3 posCheck;
+    bool subiendo = true;
+
+    public float smoothFactor = 0.5f;
+
+    void Update()
+    {
+        if (transform.position == posInicial || subiendo)
+        {
+            transform.position = Vector3.Lerp(transform.position, posArriba, Time.deltaTime * smoothFactor);
+
+        }
+        else if(transform.position == posArriba || !subiendo)
+        {
+            subiendo = false;
+            transform.position = Vector3.Lerp(transform.position, posInicial, Time.deltaTime * smoothFactor);
+        }
+    }
+}
